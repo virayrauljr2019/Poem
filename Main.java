7e1ae5612004c9027ad3f110a79aa56c35762cb4
virayrauljr2019 <42677973+virayrauljr2019@users.noreply.github.com>
2018-09-12 15:31:26 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("l(a\n\n\nle\n\naf\n\nfa\n\n\nll\n\n\ns)\n\none\n\nl\n\n\niness");
+    }
+}
